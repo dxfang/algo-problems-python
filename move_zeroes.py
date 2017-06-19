71fe17e4ed5d871951f5d5d3ff01596bc5ad174c
Daxian Fang <daxianfang@gmail.com>
2017-06-18 23:31:38 -0700

    Enhanced solution performance

--- move_zeroes.py
+++ move_zeroes.py
@@ -5,13 +5,32 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         
-        index = 0
-        tail = len(nums) - 1
+        """
+        Original solution
+        """
+
+        # index = 0
+        # tail = len(nums) - 1
         
-        while index < tail:
-            if nums[index] == 0:
-                nums.append(0)
-                del nums[index]
-                tail -= 1
-            else:
-                index += 1
+        # while index < tail:
+        #     if nums[index] == 0:
+        #         nums.append(0)
+        #         del nums[index]
+        #         tail -= 1
+        #     else:
+        #         index += 1
+
+
+        """
+        A better performance solution
+        """
+
+        non_zero_index = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+                
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0

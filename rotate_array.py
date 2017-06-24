735091ea636bf130e4ac6882c2b5966b4a53800e
Daxian Fang <daxianfang@gmail.com>
2017-06-23 23:50:04 -0700

    Added a second solution

--- rotate_array.py
+++ rotate_array.py
@@ -6,8 +6,19 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         
+        """
+        Solution 1: Rotate the list by 1 by k times
+        """
+
         itr = k % len(nums)
         
         for i in range(itr):
             nums.insert(0, nums[-1])
             nums.pop()
+
+        """
+        Solution 2: Reform the list using slice operations
+        """
+
+        itr = len(nums) - k % len(nums)        
+        nums[:] = nums[itr:] + nums[:itr]

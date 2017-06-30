1122120aca8f1b33341b4242eeb5c2ee8c6b8d6d
Daxian Fang <daxianfang@gmail.com>
2017-06-30 13:15:54 -0700

    Added pascals_triangle to repository

---
+++ pascals_triangle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+        result = [[1]]
+        
+        for i in range(1, numRows):
+            result.append(map(lambda x, y: x + y, result[-1] + [0], [0] + result[-1]))
+            
+        return result[:numRows]
